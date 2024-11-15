6beaa6353d17ce6e64318147a842ef221449d561
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-11-15 21:45:33 +0600

    added social login part and find us part

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -4,6 +4,7 @@ import NewsHeadline from "./components/NewsHeadline";
 import NavBar from "./components/NavBar";
 import LeftAside from "./components/LeftAside";
 import { Outlet } from "react-router-dom";
+import RightNav from "./components/RightNav";
 
 const MainLayout = () => {
   return (
@@ -14,7 +15,7 @@ const MainLayout = () => {
       <main className="w-11/12 mx-auto mb-8 grid lg:grid-cols-12 gap-5">
         <aside className="left col-span-3"><LeftAside /></aside>
         <section className="col-span-6"><Outlet /></section>
-        <aside className="col-span-3">right aside</aside>
+        <aside className="col-span-3"><RightNav /></aside>
       </main>
     </div>
   );
